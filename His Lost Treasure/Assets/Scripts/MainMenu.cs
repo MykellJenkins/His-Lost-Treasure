ee41798385b02b7b77b1bc8db724f7ada6f7e0c8
FlameOnBoy9090 <darkblade8921@gmail.com>
2026-01-06 15:19:23 -0500

    Added Option button and quit functions to the Main menu

--- His Lost Treasure/Assets/Scripts/MainMenu.cs
+++ His Lost Treasure/Assets/Scripts/MainMenu.cs
@@ -9,4 +9,14 @@ public class MainMenu : MonoBehaviour
   {
     SceneManager.LoadSceneAsync(1);
     }
+
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
 }
