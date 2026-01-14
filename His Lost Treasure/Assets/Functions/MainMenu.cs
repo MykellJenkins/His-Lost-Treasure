68a8728be76b9158b39ae5cbe5a6c1d39c482dad
FlameOnBoy9090 <darkblade8921@gmail.com>
2026-01-14 18:03:11 -0500

    Added Font for the main menu and worked on adding music + main menu options panel.

--- His Lost Treasure/Assets/Functions/MainMenu.cs
+++ His Lost Treasure/Assets/Functions/MainMenu.cs
@@ -5,10 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-  public void PlayGame()
+
+    public void PlayGame()
   {
     SceneManager.LoadSceneAsync(1);
-  }
+    }
 
 
     public void QuitGame()
