7a649b8309b62b6d7d35f515ee7f4d8e90d7f064
Mykell Jenkins <mykelljenkinstrina@gmail.com>
2026-01-15 18:46:47 -0500

    Camera working

---
+++ His Lost Treasure/Assets/Player Stuff/Player Scripts/Player Camera.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    void Start()
+    {
+        // Lock the cursor to the center of the screen
+        Cursor.lockState = CursorLockMode.Locked;
+
+        // Hide the cursor
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        // Unlock and show the cursor when Escape is pressed
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
