44549778192e7aaaeecb17bcae33a2339f8fcac2
Christopher Hoseclaw <bossitron424@gmail.com>
2026-01-18 15:07:13 -0500

    Updated and fixed game manager and UI

--- His Lost Treasure/Assets/Functions/GameManager.cs
+++ His Lost Treasure/Assets/Functions/GameManager.cs
@@ -33,7 +33,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Cancel"))
+        if (Input.GetKeyDown(KeyCode.P))
         {
             if(menuActive == null)
             {
