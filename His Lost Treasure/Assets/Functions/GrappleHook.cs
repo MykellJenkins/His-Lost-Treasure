de80fe7d7d7107217c089c0ec9e0dd551e9d2871
shawn <shawnmahan79@gmail.com>
2026-01-23 13:35:38 -0600

    some small adjustments to the grapple hook

--- His Lost Treasure/Assets/Functions/GrappleHook.cs
+++ His Lost Treasure/Assets/Functions/GrappleHook.cs
@@ -3,30 +3,33 @@ using UnityEngine;
 public class GrappleHook : MonoBehaviour
 {
     [SerializeField] LayerMask ignoreLayer;
-
     [SerializeField] Rigidbody rb;
 
     [SerializeField] int maxGrappleDistance; 
-    [SerializeField] int grappleSpeed; 
+    [SerializeField] int grappleSpeed;
+    
    
     Vector3 grapplePos; 
+
     bool isGrappling; 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() { }
     // Update is called once per frame
     private void Update()
     {
-        Debug.DrawRay(GameManager.Instance.playerScript.transform.position, Camera.main.transform.forward * maxGrappleDistance, Color.red);
+        
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * maxGrappleDistance, Color.red);
         HandleGrapple();
     }
     void FixedUpdate()
     {
-        HandelGrappling();
+        HandleGrappling();
     } 
     void HandleGrapple() 
     { 
-        if (Input.GetKeyDown(KeyCode.F)) 
-        { 
+        if (Input.GetKeyDown(KeyCode.F) && !isGrappling) 
+        {
             RaycastHit hit;
             if (Physics.Raycast(GameManager.Instance.playerScript.transform.position, Camera.main.transform.forward,
                 out hit, maxGrappleDistance, ~ignoreLayer) && hit.collider.CompareTag("GrapplePoint")) 
@@ -34,20 +37,23 @@ public class GrappleHook : MonoBehaviour
                 Debug.Log("Hit: " + hit.collider.name);
                 grapplePos = hit.point;
                 isGrappling = true;
+                rb.useGravity = false;
             }
         } 
     }
-    void HandelGrappling()
+    void HandleGrappling()
     {
         if (isGrappling)
         {
-            if (Vector3.Distance(rb.transform.position, grapplePos) > 0.1f)
+            if (Vector3.Distance(rb.position, grapplePos) > 0.9f)
             {
-                rb.MovePosition(Vector3.MoveTowards(rb.transform.position, grapplePos, Time.fixedDeltaTime * grappleSpeed));
+                rb.MovePosition(Vector3.MoveTowards(rb.position, grapplePos, Time.fixedDeltaTime * grappleSpeed));
             }
             else
             {
                 isGrappling = false;
+                grapplePos = Vector3.zero;
+                rb.useGravity = true;
             }
         }
       
