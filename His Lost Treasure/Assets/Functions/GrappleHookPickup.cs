f9353ad7eedd757fffd720d6bb273b8c42857228
shawn <shawnmahan79@gmail.com>
2026-01-24 21:53:12 -0600

    changed some things because it wasnt working got it working

--- His Lost Treasure/Assets/Functions/GrappleHookPickup.cs
+++ His Lost Treasure/Assets/Functions/GrappleHookPickup.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class GrappleHookPickup : MonoBehaviour
 {
+    bool havePickedup;
+    private void Update()
+    {
+        havePickedup = GameManager.Instance.player.GetComponent<GrappleHook>().GetCanGrapple();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if (!GameManager.Instance.player.GetComponent<GrappleHook>().GetCanGrapple())
+            if (havePickedup == false)
             {
                 GameManager.Instance.player.GetComponent<GrappleHook>().SetCanGrapple(true);
             }
